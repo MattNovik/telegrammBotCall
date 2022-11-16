9b4688cbc43506d75037916cf5c4c70cf3d0407b
lockdur <matvej.novik@gmail.com>
2022-11-16 14:25:52 +0300

    check

--- fetch.mjs
+++ fetch.mjs
@@ -1,5 +1,30 @@
 import 'isomorphic-fetch';
+
+let apikey = 'AIzaSyA-20iNazzzkkd4Zg4PWxHMucpuh5DUDbI';
+let search_term = 'funny';
+let lmt = '30';
+let listOfGifs = [];
+let finalGif;
+const getRandomArbitrary = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
 fetch(
-  'https://api.telegram.org/bot5674730604:AAEq34bgTEFtfKuGvRSLN55xkSZHPLFuQPM/sendAnimation?chat_id=-832187211&animation=https://media.tenor.com/NoGz1K0OrEQAAAAC/shenmue-shenmue-idiot.gif',
-  { method: 'POST' }
-);
+  'https://tenor.googleapis.com/v2/search?q=' +
+    search_term +
+    '&key=' +
+    apikey +
+    '&limit=' +
+    lmt
+)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.map((item) => {
+      listOfGifs.push(item.url);
+    });
+    finalGif = listOfGifs[getRandomArbitrary(0, listOfGifs.length - 1)];
+    return fetch(
+      'https://api.telegram.org/bot5674730604:AAEq34bgTEFtfKuGvRSLN55xkSZHPLFuQPM/sendAnimation?chat_id=-832187211&animation=' +
+        finalGif,
+      { method: 'POST' }
+    );
+  });
