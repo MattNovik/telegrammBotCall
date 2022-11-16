fb9f41de47ac0acd4dc8e243a99dd2d1638c8885
lockdur <matvej.novik@gmail.com>
2022-11-16 13:42:47 +0300

    init

---
+++ fetch.mjs
@@ -0,0 +1,5 @@
+import 'isomorphic-fetch';
+fetch(
+  'https://api.telegram.org/bot5674730604:AAEq34bgTEFtfKuGvRSLN55xkSZHPLFuQPM/sendAnimation?chat_id=-832187211&animation=https://media.tenor.com/NoGz1K0OrEQAAAAC/shenmue-shenmue-idiot.gif',
+  { method: 'POST' }
+);
